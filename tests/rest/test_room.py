09481e16340e28b0c82ad2a645a6cabc70c446a7
JichangJang <wlckd90@gmail.com>
2021-06-12 00:19:17 +0900

    Error management

--- tests/rest/test_room.py
+++ tests/rest/test_room.py
@@ -24,3 +24,21 @@ def test_get(mock_use_case, client):
     mock_use_case.assert_called()
     assert http_response.status_code == 200
     assert http_response.mimetype == "application/json"
+
+
+@mock.patch("application.rest.room.room_list_use_case")
+def test_get_with_filters(mock_use_case, client):
+    mock_use_case.return_value = ResponseSuccess(rooms)
+
+    http_response = client.get(
+        "/rooms?filter_price__gt=2&filter_price__lt=6"
+    )
+
+    assert json.loads(http_response.data.decode("UTF-8")) == [room_dict]
+
+    mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {"price__gt": "2", "price__lt": "6"}
+
+    assert http_response.status_code == 200
+    assert http_response.mimetype == "application/json"
